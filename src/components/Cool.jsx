c9b0e47a8f335fd1cad760f404197c2e7da98d2a
Joel <jitwaru2@hotmail.com>
2026-01-03 21:29:23 -0500

    mah dawg

--- src/components/Cool.jsx
+++ src/components/Cool.jsx
@@ -1,6 +1,10 @@
 function CoolDawg() {
     return (
-        <h2>This is mah dawg when he cool</h2>
+        <>
+            <h2>This is mah dawg when he cool</h2>
+
+            <img src="./src/assets/coolDogImage-02-0056567-651ab9821a4e5.png" alt="CoolDawg" />
+        </>
     )
 }
 
--- src/components/Happy.jsx
+++ src/components/Happy.jsx
@@ -1,6 +1,9 @@
 function HappyDawg() {
     return (
-        <h2>This is mah dawg when he happy</h2>
+        <>
+            <h2>This is mah dawg when he happy</h2>
+            <img src="./src/assets/happyDogImage-12-0201701-657870cb5f9ea.png" alt="HappyDawg" />
+        </>
     )
 }
 
